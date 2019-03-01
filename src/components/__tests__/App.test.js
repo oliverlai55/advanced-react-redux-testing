a0cfd0b3ecd75dbfc1ada8c21c2d164c951177d8
Oliver Lai <oliverlai55@gmail.com>
2019-03-01 18:48:52 -0500

    Add second test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
+  const wrapper = shallow(<App />);
 
-  expect(wrapped.find(CommentBox).length).toEqual(1);
+  expect(wrapper.find(CommentBox).length).toEqual(1);
 });
+
+it('shows a comment list', () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper.find(CommentList).length).toEqual(1);
+})
