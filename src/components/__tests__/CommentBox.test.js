8b9235acf742c2c65136518d0493e3ae3d473b23
Oliver Lai <oliverlai55@gmail.com>
2019-03-03 20:58:04 -0500

    Add test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -21,10 +21,25 @@ it('has a text area and a button', () => {
 it('has a text area that users can type in', () => {
   // simulate has a bunch of options to simulate
   wrapper.find('textarea').simulate('change', {
-    target: { value: 'new comment'}
-  })
+    target: { value: 'new comment' }
+  });
   // Force component re-renders so it has new value
   wrapper.update();
 
-  
+  expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+});
+
+// submitting form empties text area
+// simulate submit event on the form itself, don't try to click the button
+it('has a text area that empties after form submits', () => {
+  wrapper.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapper.update();
+
+  // expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+  wrapper.find('form').simulate('submit');
+  wrapper.update();
+
+  expect(wrapper.find('textarea').prop('value')).toEqual('');
 });
