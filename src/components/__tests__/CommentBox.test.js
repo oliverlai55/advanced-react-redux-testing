e2565092120ac21db1421fa3ad571eaeccbe36fd
Oliver Lai <oliverlai55@gmail.com>
2019-03-04 15:35:51 -0500

    Add describe statement to test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,28 +18,26 @@ it('has a text area and a button', () => {
   expect(wrapper.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  // simulate has a bunch of options to simulate
-  wrapper.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+// describe limit the scope of each beforeEach statement
+describe('the text area', () => {
+  beforeEach(() => {
+    // simulate has a bunch of options to simulate
+    wrapper.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    // Force component re-renders so it has new value
+    wrapper.update();
+  })
+
+  it('has a text area that users can type in', () => {
+    expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
   });
-  // Force component re-renders so it has new value
-  wrapper.update();
 
-  expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
-});
-
-// submitting form empties text area
-// simulate submit event on the form itself, don't try to click the button
-it('has a text area that empties after form submits', () => {
-  wrapper.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+  // submitting form empties text area
+  // simulate submit event on the form itself, don't try to click the button
+  it('has a text area that empties after form submits', () => {
+    wrapper.find('form').simulate('submit');
+    wrapper.update();
+    expect(wrapper.find('textarea').prop('value')).toEqual('');
   });
-  wrapper.update();
-
-  // expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
-  wrapper.find('form').simulate('submit');
-  wrapper.update();
-
-  expect(wrapper.find('textarea').prop('value')).toEqual('');
 });
